e6c45b4e860eece13507da775068f778aefe029c
Hritesh_Saha <hriteshsaha4@gmail.com>
2025-03-15 13:24:16 +0530

    fixed minor bug

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use("/emergency",EmergencyRoutes);
 app.use("/auth",AuthRoutes);
 
-app.get("/",()=>{
+app.get("/",(req,res)=>{
     return res.status(200).json({message:"Welcome to Emergency Service"});
 })
 
